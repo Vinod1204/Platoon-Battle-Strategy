902d815f0fa854cb119401be9a2cb19ad1169535
Vinod <vinodmurugan12@gmail.com>
2025-05-16 11:47:30 +0530

    Code Extension

--- PlatoonBattleStrategy.java
+++ PlatoonBattleStrategy.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Platoon {
     String type;
     int count;
@@ -25,14 +24,36 @@ public class PlatoonBattleStrategy {
         put("CavalryArcher", Arrays.asList("Spearmen", "HeavyCavalry"));
         put("FootArcher", Arrays.asList("Militia", "CavalryArcher"));
     }};
+    static Map<String, Map<String,Double>> terrainEffect=new HashMap<>()
+    {{
+        put("Hill", new HashMap<>() {{
+            put("CavalryArcher", 2.0);
+            put("FootArcher", 2.0);
+            put("Militia", 0.5);
+            put("Spearmen", 0.5);
+            put("LightCavalry", 0.5);
+            put("HeavyCavalry", 10.5);
+        }});
+        put("Plains", new HashMap<>() {{
+            put("CavalryArcher", 2.0);
+            put("LightCavalry", 2.0);
+            put("HeavyCavalry", 2.0);
+        }});
+        put("Muddy", new HashMap<>() {{
+            put("FootArcher", 2.0);
+            put("Militia", 2.0);
+            put("Spearmen", 2.0);
 
+        }});
+    }};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String myLine = sc.nextLine();
         String enemyLine = sc.nextLine();
-
+        String terrainLine = sc.nextLine();
         List<Platoon> myPlatoons = parsePlatoons(myLine);
         List<Platoon> enemyPlatoons = parsePlatoons(enemyLine);
+        List<String> terrains = Arrays.asList(terrainLine.split(";"));
 
         List<List<Platoon>> permutations = new ArrayList<>();
         generatePermutations(myPlatoons, 0, permutations);
@@ -40,7 +61,7 @@ public class PlatoonBattleStrategy {
         for (List<Platoon> perm : permutations) {
             int wins = 0;
             for (int i = 0; i < 5; i++) {
-                if (doesWin(perm.get(i), enemyPlatoons.get(i))) {
+                if (doesWin(perm.get(i), enemyPlatoons.get(i),terrains.get(i))) {
                     wins++;
                 }
             }
@@ -74,7 +95,7 @@ public class PlatoonBattleStrategy {
         }
     }
 
-    static boolean doesWin(Platoon mine, Platoon enemy) {
+    static boolean doesWin(Platoon mine, Platoon enemy,String terrain) {
         int myEffective = mine.count;
         int enemyEffective = enemy.count;
 
@@ -84,7 +105,11 @@ public class PlatoonBattleStrategy {
         if (hasAdvantage(enemy.type, mine.type)) {
             enemyEffective *= 2;
         }
-
+        if(terrainEffect.containsKey(terrain))
+        {
+            myEffective *= terrainEffect.get(terrain).getOrDefault(mine.type, 1.0);
+            enemyEffective *= terrainEffect.get(terrain).getOrDefault(enemy.type, 1.0);
+        }
         return myEffective > enemyEffective;
     }
 
